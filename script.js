d0fffcba6194e520fa28a78b2ea3806bffe2756a
Renz Landicho <renzrulandicho@gmail.com>
2022-10-24 17:09:46 +0800

    add: slider value now updates canvas grid count

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
 var setButtons = document.querySelectorAll('.settings__button');
+var setSlider = document.querySelector('.settings__slider');
+var setSliderInfo = document.querySelector('.settings__slider--info');
 var canvas = document.querySelector('.canvas');
-let gridCount = 16;
-let gridTotal = gridCount*gridCount;
+let sliderValue = 16;
+let gridTotal = 256;
 
-canvas.style.gridTemplateColumns = `repeat(${gridCount}, 1fr)`;
-canvas.style.gridTemplateRows = `repeat(${gridCount}, 1fr)`;
 
-setButtons[0].addEventListener('click', () => editPixel('#303030'));
-setButtons[2].addEventListener('click', () => editPixel('transparent'));
-setButtons[3].addEventListener('click', () => clearCanvas());
+function initGridCells() {
+    canvas.style.gridTemplateColumns = `repeat(${sliderValue}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${sliderValue}, 1fr)`;
+}
+
+function initSettings() {
+    setButtons[0].addEventListener('click', () => editPixels('#303030'));
+    setButtons[2].addEventListener('click', () => editPixels('transparent'));
+    setButtons[3].addEventListener('click', () => clearCanvas());
+    setSlider.addEventListener('input', () => updateCanvas());
+}
+
+function editPixels(color) {
+    let canvasPixels = canvas.childNodes;
+    
+    for (let i = 0; i < gridTotal; i++){
+        canvasPixels[i].addEventListener('mouseover', () => {
+            canvasPixels[i].style.backgroundColor = `${color}`;
+        })} 
+}
 
 function populateCanvas() {
     for (let i = 0; i < gridTotal; i++) {
         let pixelPop = document.createElement('div');
         pixelPop.classList.add('canvas--gridlines');
-    
+
         canvas.appendChild(pixelPop);
     }
-    editPixel('#303030');
-}
-
-function editPixel(color) {
-    let canvasPixels = canvas.childNodes;
-
-    for (let i = 0; i < gridTotal; i++){
-        console.log(canvasPixels[i])
-        canvasPixels[i].addEventListener('mouseover', () => {
-            canvasPixels[i].style.backgroundColor = `${color}`;
-        });
-    }
+    editPixels('#303030');
 }
 
 function clearCanvas() {
@@ -36,7 +42,19 @@ function clearCanvas() {
     populateCanvas();
 }
 
+function updateCanvas() {
+    sliderValue = setSlider.value;
+    setSliderInfo.innerHTML = `${sliderValue} x ${sliderValue}`;
+    gridTotal = sliderValue*sliderValue;
+    initGridCells();
+    clearCanvas();
+}
+
+initGridCells();
+initSettings();
 populateCanvas();
+
+
 
 
 
