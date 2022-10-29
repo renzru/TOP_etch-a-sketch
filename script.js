99ce3e8c5e3720d6f09e5664da3b6342d6ccc6d8
Renz Landicho <renzrulandicho@gmail.com>
2022-10-29 18:56:30 +0800

    feat: draw/erase on mouse drag

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ var setSliderInfo = document.querySelector('.settings__slider--info');
 var canvas = document.querySelector('.canvas');
 let sliderValue = 16;
 let gridSize = 256;
+let mouseDrag = false;
 
+document.body.onmousedown = () => mouseDrag = true;
+document.body.onmouseup = () => mouseDrag = false;
 
 function initGridCells() {
     canvas.style.gridTemplateColumns = `repeat(${sliderValue}, 1fr)`;
@@ -12,13 +15,13 @@ function initGridCells() {
 }
 
 function initSettings() {
-    setButtons[0].addEventListener('click', () => editPixels('#303030'));
-    setButtons[2].addEventListener('click', () => editPixels('transparent'));
-    setButtons[3].addEventListener('click', () => clearCanvas());
-    setSlider.addEventListener('change', () => updateCanvas());
+    setButtons[0].onclick = () => editPixels('#303030');
+    setButtons[2].onclick = () => editPixels('transparent');
+    setButtons[3].onclick = () => clearCanvas();
 
     /* updating the slider info needs a different event handler since
     the updateCanvas function lags when triggered on 'input'*/
+    setSlider.addEventListener('change', () => updateCanvas());
     setSlider.addEventListener('input', () => {
         sliderValue = setSlider.value;
         setSliderInfo.innerHTML = `${sliderValue} x ${sliderValue}`;
@@ -29,17 +32,25 @@ function editPixels(color) {
     let canvasPixels = canvas.childNodes;
     
     for (let i = 0; i < gridSize; i++){
-        canvasPixels[i].addEventListener('mouseover', () => {
+
+        canvasPixels[i].onclick = () => {
             canvasPixels[i].style.backgroundColor = `${color}`;
-        })} 
+        }
+
+        canvasPixels[i].onmouseover = () => {
+            if (mouseDrag == true) {
+                canvasPixels[i].style.backgroundColor = `${color}`;
+            }
+        }
+    } 
 }
 
 function populateCanvas() {
     for (let i = 0; i < gridSize; i++) {
-        let pixelPop = document.createElement('div');
-        pixelPop.classList.add('canvas--gridlines');
+        let canvasPixel = document.createElement('div');
+        canvasPixel.classList.add('canvas--gridlines');
 
-        canvas.appendChild(pixelPop);
+        canvas.appendChild(canvasPixel);
     }
     editPixels('#303030');
 }
